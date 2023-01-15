68de99c1136a41f2858ac18d7630a9dedf48aaa5
MUSESTARV <yxianxt@163.com>
2023-01-15 18:03:08 +0800

    更新

---
+++ src/js/BangumiList.js
@@ -0,0 +1,459 @@
+//下面是滚动图列表
+export const TheImgList = ()=> {
+	return [
+		{src: "https://s1.imagehub.cc/images/2023/01/07/2b2b916eedb5e2d8d025ac6120f4b264.jpeg", href: 'https://www.musestar.top/animation/Itainohaiya'},
+		{src: "https://s1.imagehub.cc/images/2022/12/11/48127033756a3831706e8eaa72612a48.jpeg", href: 'https://www.musestar.top/animation/TentenKakume'},
+		{src: "https://s1.imagehub.cc/images/2022/12/23/66f481396edc1415d1060b8747fafbd3.jpeg", href: 'https://www.musestar.top/animation/bdonjl'}
+	];
+}
+//下面是番剧列表
+export const TheBangumiList = ()=>{
+	return [
+		{
+			title:"魔女之旅",
+			src:"https://www.musestar.top/animation/mnzl",
+			img:"https://s1.imagehub.cc/images/2022/12/04/e50bf5b9866cded55cfd9f4596d71a82.webp",
+			releaseDate:new Date(2022,11,11),
+			classify:"冒险",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"因为太怕痛就全点防御力了第二季",
+			src:"https://www.musestar.top/animation/Itainohaiya",
+			img:"https://s1.imagehub.cc/images/2022/12/23/66f481396edc1415d1060b8747fafbd3.jpeg",
+			releaseDate:new Date(2023,1,11),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+		{
+			title:"妖幻三重奏",
+			src:"https://www.musestar.top/animation/AyakashiTriangle",
+			img:"https://s1.imagehub.cc/images/2023/01/11/1c10af10eeb0acb9bef8c508da9950f3.jpeg",
+			releaseDate:new Date(2023,1,9),
+			classify:"奇幻",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+		{
+			title:"最强阴阳师的异世界转生记",
+			src:"https://www.musestar.top/animation/SaikyouOnmyouji",
+			img:"https://s1.imagehub.cc/images/2023/01/09/e7f0523688703d5148f03d3df7f4121d.webp",
+			releaseDate:new Date(2023,1,7),
+			classify:"奇幻",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+		{
+			title:"关于我在无意间被隔壁的天使变成废柴这件事",
+			src:"https://www.musestar.top/animation/OtonarinoTenshisama",
+			img:"https://s1.imagehub.cc/images/2022/12/23/95d115413660da96bde32f6cef0e933e.jpeg",
+			releaseDate:new Date(2023,1,7),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+		{
+			title:"尼尔：自动人形",
+			src:"https://www.musestar.top/animation/NieRAutomata",
+			img:"https://s1.imagehub.cc/images/2022/12/23/dc905bc1842726a76afb441ad3abd621.jpeg",
+			releaseDate:new Date(2023,1,7),
+			classify:"战斗",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+		{
+			title:"别当欧尼酱了！",
+			src:"https://www.musestar.top/animation/bdonjl",
+			img:"https://s1.imagehub.cc/images/2022/12/11/48127033756a3831706e8eaa72612a48.jpeg",
+			releaseDate:new Date(2023,1,5),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"间谍教室",
+			src:"https://www.musestar.top/animation/Spyroom",
+			img:"https://s1.imagehub.cc/images/2023/01/11/afaa8b07965871b302c0ba2c16302ac7.webp",
+			releaseDate:new Date(2023,1,5),
+			classify:"战斗",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+		{
+			title:"生而为狗 我很幸福",
+			src:"https://www.musestar.top/animation/Inuhiro",
+			img:"https://s1.imagehub.cc/images/2023/01/04/66358a90d9678b35784b8e00f432ee57.webp",
+			releaseDate:new Date(2023,1,5),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+		{
+			title:"转生王女与天才千金的魔法革命",
+			src:"https://www.musestar.top/animation/TentenKakume",
+			img:"https://s1.imagehub.cc/images/2023/01/04/92834316efdfec96df02ce6049d159be.jpeg",
+			releaseDate:new Date(2023,1,4),
+			classify:"奇幻",
+			country:"日本",
+			state:"总话数: 预计12"
+		},
+        {
+            title:"2022年度番剧混剪",
+            src:"https://www.musestar.top/animation/2022",
+            img:"https://s1.imagehub.cc/images/2023/01/10/ceeab34da924f5454937d9ae94353929.jpeg",
+            releaseDate:new Date(2023,1,1),
+            classify:"混剪",
+            country:"中国",
+            state:"时长：5:04"
+        },
+		{
+			title:"恋爱Flops",
+			src:"https://www.musestar.top/animation/LoveFlopsProject",
+			img:"https://s1.imagehub.cc/images/2023/01/07/01611a9d9d25f613c405b179790cbf96.webp",
+			releaseDate:new Date(2022,10,12),
+			classify:"恋爱",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"电锯人",
+			src:"https://www.musestar.top/animation/ChainsawMan",
+			img:"https://s1.imagehub.cc/images/2022/12/04/045182e32256c52fd54a25124986b463.webp",
+			releaseDate:new Date(2022,10,11),
+			classify:"剧情",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"孤独摇滚！",
+			src:"https://www.musestar.top/animation/gdyg",
+			img:"https://s1.imagehub.cc/images/2022/12/04/11eeef2609b77dfdb204d3cad2fd8e62.webp",
+			releaseDate:new Date(2022,10,8),
+			classify:"学园",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"仙王的日常生活第三季",
+			src:"https://www.musestar.top/animation/xwdrcsh3",
+			img:"https://s1.imagehub.cc/images/2022/12/04/39764a6a005b62bcae4a7fcf777f21f4.webp",
+			releaseDate:new Date(2022,10,2),
+			classify:"喜剧",
+			country:"中国",
+			state:"总话数: 12"
+		},
+		{
+			title:"间谍过家家第二部分",
+			src:"https://www.musestar.top/animation/spyfamily2",
+			img:"https://s1.imagehub.cc/images/2022/12/12/e7a9c3ad075b159b70c33ced59c538bd.jpeg",
+			releaseDate:new Date(2022,10,1),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 25"
+		},
+		{
+			title:"赛博朋克：边缘行者",
+			src:"https://www.musestar.top/animation/CyberpunkEdgerunners",
+			img:"https://s1.imagehub.cc/images/2022/12/23/a5c3cf69338c65179d4a56ec6b23d9dd.jpeg",
+			releaseDate:new Date(2022,9,13),
+			classify:"动作",
+			country:"日本",
+			state:"总话数: 10"
+		},
+		{
+			title:"最近雇的女仆有点怪",
+			src:"https://www.musestar.top/animation/zjgdnpydg",
+			img:"https://s1.imagehub.cc/images/2022/12/04/3d0e9d7e1d93af386e4b1761a7e869e1.jpeg",
+			releaseDate:new Date(2022,7,23),
+			classify:"恋爱",
+			country:"日本",
+			state:"总话数: 11"
+		},
+		{
+			title:"彻夜之歌",
+			src:"https://www.musestar.top/animation/cyzg",
+			img:"https://s1.imagehub.cc/images/2022/12/04/f41171377b6a2e4d92f4a314c20f60f3.jpeg",
+			releaseDate:new Date(2022,7,7),
+			classify:"恋爱",
+			country:"日本",
+			state:"总话数: 13"
+		},
+		{
+			title:"即使如此依旧步步进逼",
+			src:"https://www.musestar.top/animation/bbjb",
+			img:"https://s1.imagehub.cc/images/2022/12/04/f308d56f4539a435f1529c0e7dfcce86.webp",
+			releaseDate:new Date(2022,7,7),
+			classify:"恋爱",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"继母的拖油瓶是我的前女友",
+			src:"https://www.musestar.top/animation/jmdtypswdqny",
+			img:"https://s1.imagehub.cc/images/2022/12/04/b06713e59bd3ad99e35efa0eb3c65c93.jpeg",
+			releaseDate:new Date(2022,7,6),
+			classify:"恋爱",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"异世界舅舅",
+			src:"https://www.musestar.top/animation/ysjjj",
+			img:"https://s1.imagehub.cc/images/2022/12/04/368fda8f8e248d349f2a74c496351b6c.webp",
+			releaseDate:new Date(2022,7,6),
+			classify:"奇幻",
+			country:"日本",
+			state:"总话数: 13"
+		},
+		{
+			title:"契约之吻",
+			src:"https://www.musestar.top/animation/EngageKiss",
+			img:"https://s1.imagehub.cc/images/2022/12/04/f01f3a837b8319f2788d26ac24a688e2.webp",
+			releaseDate:new Date(2022,7,4),
+			classify:"恋爱",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"天籁人偶",
+			src:"https://www.musestar.top/animation/tlro",
+			img:"https://s1.imagehub.cc/images/2022/12/04/65868df216b11eb04ce55d622a3a417d.jpeg",
+			releaseDate:new Date(2022,7,4),
+			classify:"恋爱",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"莉可丽丝",
+			src:"https://www.musestar.top/animation/LycorisRecoil",
+			img:"https://s1.imagehub.cc/images/2022/12/04/42b73b8a68d48adb17e41b8907af0713.jpeg",
+			releaseDate:new Date(2022,7,2),
+			classify:"战斗",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"间谍过家家",
+			src:"https://www.musestar.top/animation/spyfamily",
+			img:"https://s1.imagehub.cc/images/2022/12/04/d63f2f091f4d2b6a6c80592e0dff8836.webp",
+			releaseDate:new Date(2022,4,9),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"古见同学有交流障碍症第二季",
+			src:"https://www.musestar.top/animation/gjtxyjlzaz2",
+			img:"https://s1.imagehub.cc/images/2022/12/04/b05576bbb86578c63d921b588e2c3860.jpeg",
+			releaseDate:new Date(2022,4,6),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"派对浪客诸葛孔明",
+			src:"https://www.musestar.top/animation/pdlk",
+			img:"https://s1.imagehub.cc/images/2022/12/04/b072d3e8462778c0b79d2774d65a800b.jpeg",
+			releaseDate:new Date(2022,4,6),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"古见同学有交流障碍症",
+			src:"https://www.musestar.top/animation/gjtxyjlzaz",
+			img:"https://s1.imagehub.cc/images/2022/12/04/2b8f250d97b66f540be302f6d0f3edef.jpeg",
+			releaseDate:new Date(2021,10,5),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"某科学的超电磁炮T",
+			src:"https://www.musestar.top/animation/Railgun3",
+			img:"https://s1.imagehub.cc/images/2022/12/04/b0217f15c74aee33496addf8e0a6dc0f.webp",
+			releaseDate:new Date(2020,1,11),
+			classify:"战斗",
+			country:"日本",
+			state:"总话数: 25"
+		},
+		{
+			title:"紫罗兰永恒花园",
+			src:"https://www.musestar.top/animation/VioletEvergarden",
+			img:"https://s1.imagehub.cc/images/2022/12/04/8b1e7891ab2689c4e4d013ea42bdcb1c.jpeg",
+			releaseDate:new Date(2018,1,10),
+			classify:"治愈",
+			country:"日本",
+			state:"总话数: 14"
+		},
+		{
+			title:"埃罗芒阿老师",
+			src:"https://www.musestar.top/animation/almals",
+			img:"https://s1.imagehub.cc/images/2022/12/04/965c1d52b020e5d395251edf9dca5e1e.webp",
+			releaseDate:new Date(2017,4,9),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"珈百璃的堕落",
+			src:"https://www.musestar.top/animation/Gabriel",
+			img:"https://s1.imagehub.cc/images/2022/12/04/2c261ab34fc3bdda33d74cda42437207.webp",
+			releaseDate:new Date(2017,1,9),
+			classify:"喜剧",
+			country:"日本",
+			state:"总话数: 12"
+		},
+		{
+			title:"Charlotte",
+			src:"https://www.musestar.top/animation/Charlotte",
+			img:"https://s1.imagehub.cc/images/2022/12/04/62a0cdb4b5cda7801d172dc850042ce3.webp",
+			releaseDate:new Date(2015,7,4),
+			classify:"治愈",
+			country:"日本",
+			state:"总话数: 13"
+		},
+		{
+			title:"某科学的超电磁炮S",
+			src:"https://www.musestar.top/animation/biliRailgun2",
+			img:"https://s1.imagehub.cc/images/2022/12/04/af83638140ea696e0087c3ffb5251f0e.webp",
+			releaseDate:new Date(2013,4,12),
+			classify:"战斗",
+			country:"日本",
+			state:"总话数: 24"
+		},
+		{
+			title:"某科学的超电磁炮",
+			src:"https://www.musestar.top/animation/biliRailgun",
+			img:"https://s1.imagehub.cc/images/2022/12/04/95cb124d7298b75a885417c9a9a37e4a.webp",
+			releaseDate:new Date(2009,10,2),
+			classify:"战斗",
+			country:"日本",
+			state:"总话数: 24"
+		}
+	]
+}
+//下面是剧场列表
+export const TheBangumiMoveList = ()=>{
+	return [
+		{
+			title:"你好世界",
+			src:"https://www.musestar.top/film/HelloWorld",
+			img:"https://s1.imagehub.cc/images/2022/12/04/ab787edbfcdc71970465ac43e1bf36dd.webp",
+			releaseDate:new Date(2021,6,11),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 1:37:43"
+		},
+		{
+			title:"铃芽户缔",
+			src:"https://www.musestar.top/film/lyhd",
+			img:"https://s1.imagehub.cc/images/2022/12/04/016fecf9d2293894e6afdad40f8d07cc.webp",
+			releaseDate:new Date(2022,11,11),
+			classify:"奇幻",
+			country:"日本",
+			state:"影片时长: x:xx:xx"
+		},
+		{
+			title:"通往夏天的隧道,再见的出口",
+			src:"https://www.musestar.top/film/twxtdsd",
+			img:"https://s1.imagehub.cc/images/2022/12/04/3315a902ab17bfb4922cf350437e9877.webp",
+			releaseDate:new Date(2022,9,9),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: x:xx:xx"
+		},
+		{
+			title:"雀斑公主",
+			src:"https://www.musestar.top/film/Belle",
+			img:"https://s1.imagehub.cc/images/2022/12/04/c78ee11ac65ff93ab2a24dbf6e7bf00a.webp",
+			releaseDate:new Date(2022,8,26),
+			classify:"冒险",
+			country:"日本",
+			state:"影片时长: 2:01:13"
+		},
+		{
+			title:"五等分的新娘剧场版",
+			src:"https://www.musestar.top/film/wdfdxnjcb",
+			img:"https://s1.imagehub.cc/images/2022/12/04/c40ecd5b8724a19e3fe92f5b964e5159.webp",
+			releaseDate:new Date(2022,5,20),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 2:16:18"
+		},
+		{
+			title:"紫罗兰永恒花园剧场版",
+			src:"https://www.musestar.top/film/VioletEvergardenjcb",
+			img:"https://s1.imagehub.cc/images/2022/12/13/210c13b6a5e04426607efe2e74f1e50d.jpeg",
+			releaseDate:new Date(2020,9,18),
+			classify:"剧情",
+			country:"日本",
+			state:"影片时长: 2:20:31"
+		},
+		{
+			title:"天气之子",
+			src:"https://www.musestar.top/film/WeatheringWithYou",
+			img:"https://s1.imagehub.cc/images/2022/12/13/2a24ba8501ca17c8040cab0f2dc003a1.jpeg",
+			releaseDate:new Date(2019,11,1),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 1:52:10"
+		},
+		{
+			title:"青春猪头少年不会梦到怀梦美少女",
+			src:"https://www.musestar.top/film/ydmyjcb",
+			img:"https://s1.imagehub.cc/images/2022/12/13/2cc28b19836842148c2aadb7e04c5524.webp",
+			releaseDate:new Date(2019,9,15),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 1:52:10"
+		},
+		{
+			title:"永远与自动手记人偶",
+			src:"https://www.musestar.top/film/VioletEvergardenwz",
+			img:"https://s1.imagehub.cc/images/2022/12/13/8c4f159e0152e4bc561580a5e0249a10.jpeg",
+			releaseDate:new Date(2019,9,6),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 1:52:10"
+		},
+		{
+			title:"你的名字",
+			src:"https://www.musestar.top/film/YourName",
+			img:"https://s1.imagehub.cc/images/2022/12/13/dd9530266383767ed8f1af0848064c45.jpeg",
+			releaseDate:new Date(2016,12,2),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 1:46:40"
+		},
+		{
+			title:"言叶之庭",
+			src:"https://www.musestar.top/film/yyzt",
+			img:"https://s1.imagehub.cc/images/2022/12/13/fbf0653e1e1dbf6d04fe46b92dfe0ed3.webp",
+			releaseDate:new Date(2013,5,31),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 46:04"
+		},
+		{
+			title:"魔法禁书目录剧场版",
+			src:"https://www.musestar.top/film/Indexjcb",
+			img:"https://s1.imagehub.cc/images/2022/12/13/a55a12f25288192ced3fdc9b6692e6ab.webp",
+			releaseDate:new Date(2013,2,23),
+			classify:"战斗",
+			country:"日本",
+			state:"影片时长: 1:30:06"
+		},
+		{
+			title:"秒速五厘米",
+			src:"https://www.musestar.top/film/5cm",
+			img:"https://s1.imagehub.cc/images/2022/12/13/54553e34898877680a2115a1d8d4c6e3.webp",
+			releaseDate:new Date(2007,3,3),
+			classify:"恋爱",
+			country:"日本",
+			state:"影片时长: 1:02:44"
+		},
+	]
+}
