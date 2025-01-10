aa2ac96579d31f9e0a9c168031d39ec42a17ab84
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2025-01-10 19:37:11 +0800

    Update BangumiList.js

--- src/js/BangumiList.js
+++ src/js/BangumiList.js
@@ -1,10 +1,10 @@
 //下面是滚动图列表
 export const TheImgList = ()=> {
 	return [
-		{src: "https://s1.imagehub.cc/images/2024/08/18/69a776cb5468e12bb38866b0f7332b0d.jpg", href: '/animation/Orb/01'},
-		{src: "https://s1.imagehub.cc/images/2024/10/07/a730387be806bf403a862b060a106fed.jpg", href: '/animation/re0s3/01'},
-		{src: "https://s1.imagehub.cc/images/2024/10/01/0065636f82a0d5f286d9f3ccec7b823d.jpg", href: '/animation/bleach3/27'},
-		{src: "https://s1.imagehub.cc/images/2024/10/01/d4ca8b6f87ec4ee38e90d9f4c116508c.jpg", href: '/animation/SeireiGensouki2/01'},
+		{src: "https://s1.imagehub.cc/images/2024/12/16/d6fe24d2d22818bff757b88a6926353c.jpg", href: '/animation/hyakkano2/01'},
+		{src: "https://s1.imagehub.cc/images/2024/12/08/4448e7557b33850ac40959b8ac720130.jpg", href: '/animation/SoloLeveling2/01'},
+		{src: "https://s1.imagehub.cc/images/2024/12/16/cd4ef51d8852f35db8764ce2c934e5f6.jpg", href: '/animation/AveMujica/01'},
+		{src: "https://s1.imagehub.cc/images/2024/12/16/bebaa3b39e403aeb1b0e45c73575e564.webp", href: '/animation/hanakokun2/01'},
 	];
 }
 //下面是番剧列表
@@ -14,16 +14,16 @@ export const TheBangumiList = ()=>{
 			title:"镇站之宝-魔女之旅",
 			src:"/animation/mnzl",
 			img:"https://s1.imagehub.cc/images/2024/05/02/8ab51680d1d65f4af22338dff9712218.png",
-			releaseDate:"2020,10,2",
+			releaseDate:"2020年10月2日",
 			classify:"治愈",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"搜索",
 			src:"https://video.musestar.cc/",
 			img:"https://api.anosu.top/api/?sort=all",
-			releaseDate:"2077,1,1",
+			releaseDate:"2077月1月1日",
 			classify:"自定义",
 			country:"搜索",
 			state:"第三方播放源在线播放"
@@ -32,70 +32,70 @@ export const TheBangumiList = ()=>{
 			title:"地缚少年花子君 第二季",
 			src:"/animation/hanakokun2/01",
 			img:"https://s1.imagehub.cc/images/2024/12/16/cd4ef51d8852f35db8764ce2c934e5f6.jpg",
-			releaseDate:"2025,1",
+			releaseDate:"2025年1月12日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(更新中)总话数: 12"
-		},
-		{
-			title:"我独自升级 第二季",
-			src:"/animation/SoloLeveling2/01",
-			img:"https://s1.imagehub.cc/images/2024/12/16/d6fe24d2d22818bff757b88a6926353c.jpg",
-			releaseDate:"2025,1",
-			classify:"奇幻",
-			country:"日本",
-			state:"(更新中)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"超超超超超喜欢你的100个女朋友 第二季",
 			src:"/animation/hyakkano2/01",
 			img:"https://s1.imagehub.cc/images/2024/12/16/bebaa3b39e403aeb1b0e45c73575e564.webp",
-			releaseDate:"2025,1,12",
+			releaseDate:"2025年1月12日",
 			classify:"喜剧",
 			country:"日本",
-			state:"(更新中)总话数: 12"
+			state:"总话数: 12"
+		},
+		{
+			title:"我独自升级 第二季",
+			src:"/animation/SoloLeveling2/01",
+			img:"https://s1.imagehub.cc/images/2024/12/16/d6fe24d2d22818bff757b88a6926353c.jpg",
+			releaseDate:"2025年1月4日",
+			classify:"奇幻",
+			country:"日本",
+			state:"总话数: 13"
 		},
 		{
 			title:"BanG Dream! Ave Mujica",
 			src:"/animation/AveMujica/01",
 			img:"https://s1.imagehub.cc/images/2024/12/08/4448e7557b33850ac40959b8ac720130.jpg",
-			releaseDate:"2025,1,2",
+			releaseDate:"2025年1月2日",
 			classify:"音乐",
 			country:"日本",
-			state:"(更新中)总话数: 12"
+			state:"总话数: 13"
 		},
 		{
 			title:"精灵幻想记 第二季",
 			src:"/animation/SeireiGensouki2/01",
 			img:"https://s1.imagehub.cc/images/2024/10/01/0065636f82a0d5f286d9f3ccec7b823d.jpg",
-			releaseDate:"2024,10,8",
+			releaseDate:"2024年10月8日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(更新中)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"死神 千年血战篇 -相克谭-",
 			src:"/animation/bleach3/27",
 			img:"https://s1.imagehub.cc/images/2024/10/07/a730387be806bf403a862b060a106fed.jpg",
-			releaseDate:"2024,10,5",
+			releaseDate:"2024年10月5日",
 			classify:"战斗",
 			country:"日本",
-			state:"(更新中)总话数: 13"
+			state:"总话数: 13"
 		},
 		{
 			title:"地。―关于地球的运动",
 			src:"/animation/Orb/01",
 			img:"https://s1.imagehub.cc/images/2024/10/01/d4ca8b6f87ec4ee38e90d9f4c116508c.jpg",
-			releaseDate:"2024,10,5",
+			releaseDate:"2024年10月5日",
 			classify:"科学",
 			country:"日本",
-			state:"(更新中)总话数: 未知"
+			state:"总话数: 25"
 		},
 		{
 			title:"盒中之海",
 			src:"/film/hzzh",
 			img:"https://s1.imagehub.cc/images/2024/10/05/722b48cfc6e2dc3a6176e240bfe38c4d.png",
-			releaseDate:"2024,10,3",
+			releaseDate:"2024年10月3日",
 			classify:"奇幻",
 			country:"中国",
 			state:"影片时长: 2:09:31"
@@ -104,16 +104,16 @@ export const TheBangumiList = ()=>{
 			title:"Re:从零开始的异世界生活 第三季",
 			src:"/animation/re0s3/01",
 			img:"https://s1.imagehub.cc/images/2024/08/18/69a776cb5468e12bb38866b0f7332b0d.jpg",
-			releaseDate:"2024,10,2",
+			releaseDate:"2024年10月2日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(更新中)总话数: 16"
+			state:"总话数: 16"
 		},
 		{
 			title:"通往夏天的隧道,再见的出口",
 			src:"/film/NatsuenoTunnel",
 			img:"https://s1.imagehub.cc/images/2024/08/19/b68cbf5ff34a36fe57d3ce0d06e4870a.png",
-			releaseDate:"2024,8,30",
+			releaseDate:"2024年8月30日",
 			classify:"恋爱",
 			country:"日本",
 			state:"影片时长: 1:22:39"
@@ -122,52 +122,52 @@ export const TheBangumiList = ()=>{
 			title:"败犬女主太多了！",
 			src:"/animation/makeine",
 			img:"https://s1.imagehub.cc/images/2024/07/26/02fa0219d89963cdffa13b4d18ccaf3c.jpeg",
-			releaseDate:"2024,7,14",
+			releaseDate:"2024年7月14日",
 			classify:"喜剧",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"鹿乃子乃子虎视眈眈",
 			src:"/animation/shikanoko",
 			img:"https://s1.imagehub.cc/images/2024/07/07/f02eaaca3d092dfbf69a4ad9974683c9.jpeg",
-			releaseDate:"2024,7,3",
+			releaseDate:"2024年7月3日",
 			classify:"喜剧",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"我推的孩子 第二季",
 			src:"/animation/oshinoko2",
 			img:"https://s1.imagehub.cc/images/2024/08/22/83ac04dacb9b4d9a4c68403f2edf1de0.jpg",
-			releaseDate:"2024,7,3",
+			releaseDate:"2024年7月3日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 24"
+			state:"总话数: 24"
 		},
 		{
 			title:"不时轻声地以俄语遮羞的邻座艾莉同学",
 			src:"/animation/roshidere",
 			img:"https://s1.imagehub.cc/images/2024/05/18/79b3a87e77a21f5bebc01183b2415dc9.webp",
-			releaseDate:"2024,7,3",
+			releaseDate:"2024年7月3日",
 			classify:"爱情",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"鬼灭之刃 第四季 柱训练篇",
 			src:"/animation/kimetsu4",
 			img:"https://s1.imagehub.cc/images/2024/04/03/79fa16f63e2969b1b008306464f147f7.png",
-			releaseDate:"2024,5,12",
+			releaseDate:"2024年5月12日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 8"
+			state:"总话数: 8"
 		},
 		{
 			title:"间谍过家家 代号：白",
 			src:"/film/spyfamilycode",
 			img:"https://s1.imagehub.cc/images/2023/09/30/spyfamilycode.jpeg",
-			releaseDate:"2023,4,30",
+			releaseDate:"2023年4月30日",
 			classify:"喜剧",
 			country:"日本",
 			state:"影片时长: 1:50:01"
@@ -176,61 +176,61 @@ export const TheBangumiList = ()=>{
 			title:"约会大作战 第五季",
 			src:"/animation/DateALive5",
 			img:"https://s1.imagehub.cc/images/2024/04/03/f540b1992cc0ac37ecfa29f5d02120a5.jpeg",
-			releaseDate:"2024,4,10",
+			releaseDate:"2024年4月10日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"为美好的世界献上祝福！ 第三季",
 			src:"/animation/konosuba3",
 			img:"https://s1.imagehub.cc/images/2024/04/03/1beffc411899eecd7a5085ead6859cb4.jpeg",
-			releaseDate:"2024,4,10",
+			releaseDate:"2024年4月10日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 11"
+			state:"总话数: 11"
 		},
 		{
 			title:"夜晚的水母不会游泳",
 			src:"/animation/yorukura",
 			img:"https://s1.imagehub.cc/images/2024/04/19/94cb2ee4ca48478b87f684ecdfa74156.jpeg",
-			releaseDate:"2024,4,7",
+			releaseDate:"2024年4月7日",
 			classify:"群像",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"无职转生Ⅱ Part2",
 			src:"/animation/mushokutensei2",
 			img:"https://s1.imagehub.cc/images/2024/04/03/8c251884ab46fb4841eff6cac7e5dc2a.jpeg",
-			releaseDate:"2024,4,7",
+			releaseDate:"2024年4月7日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"葬送的芙莉莲 第二季度",
 			src:"/animation/FrierenBeyondJourneysEnd",
 			img:"https://s1.imagehub.cc/images/2023/12/29/01191f75f4d21914dd389a46f6c42e02.webp",
-			releaseDate:"2024,1,5",
+			releaseDate:"2024年1月5日",
 			classify:"治愈",
 			country:"日本",
-			state:"(已完结)总话数: 28"
+			state:"总话数: 28"
 		},
 		{
 			title:"憧憬成为魔法少女",
 			src:"/animation/mahoako",
 			img:"https://s1.imagehub.cc/images/2024/01/01/622396e5acdeffcc1307614f48396be5.webp",
-			releaseDate:"2024,1,3",
+			releaseDate:"2024年1月3日",
 			classify:"魔法",
 			country:"日本",
-			state:"(已完结)总话数: 13"
+			state:"总话数: 13"
 		},
 		{
 			title:"青春猪头少年不会梦到红书包女孩",
 			src:"/film/knapsack",
 			img:"https://s1.imagehub.cc/images/2024/07/04/af4907bcf6d53279c6aeeda32a3203f5.jpeg",
-			releaseDate:"2023,12,1",
+			releaseDate:"2023年12月1日",
 			classify:"剧情",
 			country:"日本",
 			state:"影片时长: 1:14:54"
@@ -239,61 +239,61 @@ export const TheBangumiList = ()=>{
 			title:"间谍过家家 第二季",
 			src:"/animation/spyfamilys2",
 			img:"https://s1.imagehub.cc/images/2023/09/29/spyfamilys2.jpeg",
-			releaseDate:"2023,10,7",
+			releaseDate:"2023年10月7日",
 			classify:"喜剧",
 			country:"日本",
-			state:"(已完结)总话数: 13"
+			state:"总话数: 13"
 		},
 		{
 			title:"家里蹲吸血姬的苦闷",
 			src:"/animation/hikikomari",
 			img:"https://s1.imagehub.cc/images/2023/09/29/hikikomari.jpeg",
-			releaseDate:"2023,10,7",
+			releaseDate:"2023年10月7日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"葬送的芙莉莲 第一季度",
 			src:"/animation/FrierenBeyondJourneysEnd",
 			img:"https://s1.imagehub.cc/images/2023/12/29/74550f2b6a424fd34b77d010533c98c2.webp",
-			releaseDate:"2023,9,29",
+			releaseDate:"2023年9月29日",
 			classify:"治愈",
 			country:"日本",
-			state:"(已完结)总话数: 28"
+			state:"总话数: 28"
 		},
 		{
 			title:"白圣女与黑牧师",
 			src:"/animation/shiroseijyo",
 			img:"https://s1.imagehub.cc/images/2023/08/05/shiroseijyo.webp",
-			releaseDate:"2023,7,12",
+			releaseDate:"2023年7月12日",
 			classify:"恋爱",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"僵尸百分百",
 			src:"/animation/zom100",
 			img:"https://s1.imagehub.cc/images/2023/07/18/zom100.webp",
-			releaseDate:"2023,7,9",
+			releaseDate:"2023年7月9日",
 			classify:"喜剧",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"无职转生Ⅱ Part1",
 			src:"/animation/mushokutensei",
 			img:"https://s1.imagehub.cc/images/2023/07/18/mushokutensei.webp",
-			releaseDate:"2023,7,2",
+			releaseDate:"2023年7月2日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"青春期猪头少年不会梦到娇怜外出妹",
 			src:"/film/odekake",
 			img:"https://s1.imagehub.cc/images/2023/09/30/odekake.jpeg",
-			releaseDate:"2023,6,23",
+			releaseDate:"2023年6月23日",
 			classify:"剧情",
 			country:"日本",
 			state:"影片时长: 1:13:53"
@@ -302,34 +302,34 @@ export const TheBangumiList = ()=>{
 			title:"我推的孩子",
 			src:"/animation/IchigoProduction",
 			img:"https://s1.imagehub.cc/images/2023/04/14/a095977c44c9dc5232e57672d3eed48e.webp",
-			releaseDate:"2023,4,12",
+			releaseDate:"2023年4月12日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 11"
+			state:"总话数: 11"
 		},
 		{
 			title:"别当欧尼酱了！",
 			src:"/animation/bdonjl",
 			img:"https://s1.imagehub.cc/images/2022/12/11/48127033756a3831706e8eaa72612a48.jpeg",
-			releaseDate:"2023,1,5",
+			releaseDate:"2023年1月5日",
 			classify:"喜剧",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"转生王女与天才千金的魔法革命",
 			src:"/animation/TentenKakume",
 			img:"https://s1.imagehub.cc/images/2023/01/04/92834316efdfec96df02ce6049d159be.jpeg",
-			releaseDate:"2023,1,4",
+			releaseDate:"2023年1月4日",
 			classify:"奇幻",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},
 		{
 			title:"铃芽之旅",
 			src:"/film/SuzumenoTojimari",
 			img:"https://s1.imagehub.cc/images/2022/12/04/016fecf9d2293894e6afdad40f8d07cc.webp",
-			releaseDate:"2022,11,11",
+			releaseDate:"2022年11月11日",
 			classify:"奇幻",
 			country:"日本",
 			state:"影片时长: 2:01:28"
@@ -338,7 +338,7 @@ export const TheBangumiList = ()=>{
 			title:"你好世界",
 			src:"/film/HelloWorld",
 			img:"https://s1.imagehub.cc/images/2022/12/04/ab787edbfcdc71970465ac43e1bf36dd.webp",
-			releaseDate:"2021,6,11",
+			releaseDate:"2021年6月11日",
 			classify:"恋爱",
 			country:"日本",
 			state:"影片时长: 1:37:43"
@@ -347,10 +347,10 @@ export const TheBangumiList = ()=>{
 			title:"魔女之旅",
 			src:"/animation/mnzl",
 			img:"https://s1.imagehub.cc/images/2022/12/04/e50bf5b9866cded55cfd9f4596d71a82.webp",
-			releaseDate:"2020,10,2",
+			releaseDate:"2020年10月2日",
 			classify:"治愈",
 			country:"日本",
-			state:"(已完结)总话数: 12"
+			state:"总话数: 12"
 		},	]
 }
 //下面是剧场列表
